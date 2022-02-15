5f2cb5160dd7902dea269c589964ace81e02e56d
Hoon Lee <jiroh618@gmail.com>
2022-02-16 00:32:38 +0900

    정수 내림차순으로 배치하기 https://programmers.co.kr/learn/courses/30/lessons/12933

---
+++ practice/if.py
@@ -0,0 +1,106 @@
+# https://info-lab.tistory.com/303
+# case1
+animal = 'dog'
+cat = 'cat'
+dog = 'dog'
+ret = None
+
+# 1. if 문 #
+if animal is dog:
+    ret = dog
+print("Default: " + ret)
+
+if animal is dog: ret = dog
+print("One-Line: " + ret)
+
+
+# [output]
+# Default: dog
+# One-Line: dog
+
+
+# case2
+cat = 'cat'
+dog = 'dog'
+ret = None
+
+# 2. if - eles 문 #
+animal = 'cat'
+if animal is dog:
+    ret = dog
+else:
+    ret = cat
+print("Default: " + ret)
+
+ret = dog if animal is dog else cat
+print("One-Line: " + ret)
+
+# animal 변수값 변경 #
+animal = 'dog'
+if animal is dog:
+    ret = dog
+else:
+    ret = cat
+print("Default: " + ret)
+
+ret = dog if animal is dog else cat
+print("One-Line: " + ret)
+
+# [output]
+# Default: cat
+# One-Line: cat
+# Default: dog
+# One-Line: dog
+
+#case 3
+cat = 'cat'
+dog = 'dog'
+cow = 'cow'
+ret = None
+
+# 3. if - elif - eles 문 #
+animal = 'cow'
+if animal is dog:
+    ret = dog
+elif animal is cat:
+    ret = cat
+else:
+    ret = cow
+print("Default: " + ret)
+
+ret = dog if animal is dog else cat if animal is cat else cow
+print("One-Line: " + ret)
+
+# animal 변수값 변경 #
+animal = 'cat'
+if animal is dog:
+    ret = dog
+elif animal is cat:
+    ret = cat
+else:
+    ret = cow
+print("Default: " + ret)
+
+ret = dog if animal is dog else cat if animal is cat else cow
+print("One-Line: " + ret)
+
+# animal 변수값 변경 #
+animal = 'dog'
+if animal is dog:
+    ret = dog
+elif animal is cat:
+    ret = cat
+else:
+    ret = cow
+print("Default: " + ret)
+
+ret = dog if animal is dog else cat if animal is cat else cow
+print("One-Line: " + ret)
+
+# [output]
+# Default: cow
+# One-Line: cow
+# Default: cat
+# One-Line: cat
+# Default: dog
+# One-Line: dog
