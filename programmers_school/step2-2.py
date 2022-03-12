efb0c6d47f886b1aa9dea91969bdf069a202d861
Hoon Lee <jiroh618@gmail.com>
2022-03-12 21:40:04 +0900

    Step 2-2, checking code on 12 MAR

--- programmers_school/step2-2.py
+++ programmers_school/step2-2.py
@@ -92,6 +92,28 @@ def solution(N, number):
     return answer
 
 
+# 해설
+def sol(N, number):
+    s = [set() for x in range(8)]
+    for i, x in enumerate (s, start=1):
+        x.add(int(str(N) * i))
+    print(s) # [{5}, {55}, {555}, {5555}, {55555}, {555555}, {5555555}, {55555555}]
+    for i in range(1, len(s)):
+        for j in range(i):
+            for op1 in s[j]:
+                for op2 in s[i-j-1]:
+                    s[i].add(op1 + op2)
+                    s[i].add(op1 - op2)
+                    s[i].add(op1 * op2)
+                    if op2 != 0:
+                        s[i].add(op1 // op2)
+        if number in s[i]:
+            answer = i + 1
+            break
+    print(x)
+    return answer
+
+
 # print(f'사용횟수는 {solution()}')
 print(f'사용횟수는 {solution(2,22222)}')
 # print(f'사용횟수는 {solution(9,600)}')
