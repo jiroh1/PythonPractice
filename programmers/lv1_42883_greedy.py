e821c4a5e8c2004c78a6088e27f39e863c209475
Hoon Lee <jiroh618@gmail.com>
2022-12-24 19:14:25 +0900

    add instructor's answer

--- programmers/lv1_42883_greedy.py
+++ programmers/lv1_42883_greedy.py
@@ -13,7 +13,7 @@ k= 4
 def solution(number, k): # 시간초과 실패
     return max(list(map(''.join, itertools.combinations(number, k))))
 
-# 최종 답안
+# 최종 제출 답안
 def solution1(number, k):
     stack = [number[0]]
     for num in number[1:]:
@@ -25,9 +25,50 @@ def solution1(number, k):
         stack = stack[:-k]
     return ''.join(stack)
 
+# 강사님
+
+def solution2(number, k):
+    answer = ''
+    # 문자열 내 요소를 숫자로 변환
+    number = list(map(int, number))
+
+    now_index = 0
+    # 배열 길이 - k만큼 돌고 만약 남으면, 더이상 볼 필요 없음
+    for i in range(len(number) - k):
+        # 내가 볼 수 있는 부분만 (k+1만큼만 가져옴)
+        remains = number[now_index: now_index + k + 1]
+        max_value = -1
+        max_index = 0
+        # 해당 부분 확인하면서 가장 큰 값, index 확인
+        for j, v in enumerate(remains):
+            # 만약 현재 값이 저장해둔 max_value보다 크면,
+            if v > max_value:
+                # 최신화
+                max_value = v
+                max_index = j
+            # 단일자리에서 가장 큰 수는 9이기 때문에, 바로 break
+            if max_value == 9: break
+
+        # nox_index, k, answer 최신화
+        # max_index는 remains의 index니까 (0부터 시작하기 때문에)
+        # now_index + max_index 그 다음꺼부터 보기 위해 +1 해줌
+        now_index += max_index + 1
+        k -= max_index
+        answer += str(max_value)
+
+    return answer
+
 
 print(solution1(number,k))
 
+"""
+풀이.
+number 를 k만큼 탐색? 완전 탐색 한다면 k만큼이면 시간초과가 날 것이라는 것을 생각해야 함.
+1. k개의 숫자를 제거
+2. 순서는 보장
+  
+
+"""
 """
 첫 풀이 과정에서 시간초과가 나서 다른 방법을 찾았음
 stack 을 사용하여 진행하는 것으로 다시 진행함 
