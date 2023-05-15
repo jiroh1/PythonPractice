ab2769cfeee70d8e86fac4b75f5044f0ac2bb48f
Hoon Lee <jiroh618@gmail.com>
2023-05-16 02:21:40 +0900

    update goorm_algorithm prime number with Sieve

--- goorm/week2_소수찾기.py
+++ goorm/week2_소수찾기.py
@@ -20,6 +20,7 @@
 # n=7
 # print(Sieve(n))
 
+# 제출 답안
 import sys
 input = sys.stdin.readline
 
@@ -27,12 +28,10 @@ def Sieve(N):
     is_prime = [1 for _ in range(N + 1)]
     prime = []
     for i in range(2, N + 1):
-        print('for,',i, is_prime[i])
         if not is_prime[i]:
             continue
         prime.append(i)
         for j in range(2 * i, N + 1, i):
-            print('2nd for,', j)
             is_prime[j] = 0
     return prime
 
@@ -41,8 +40,8 @@ A = [0] + list(map(int, input().split()))
 ans = 0
 
 primes = Sieve(N)
+# 수열의 앞에서 소수 번째에 위치한 값
 for prime in primes:
-    print('**',A[prime])
     ans += A[prime]
         
 print(ans)
