bd0bf2fe3e7d4df1e97f61da721c2404fed9a769
Hoon Lee <jiroh618@gmail.com>
2023-05-27 22:12:10 +0900

    update goorm_algorithm additional submission

--- goorm/week4_제곱암호.py
+++ goorm/week4_제곱암호.py
@@ -2,21 +2,6 @@
 6
 z2a1y5
 '''
-N = int(input())
-S = list(input().rstrip())
-
-a_list=[a for a in S[::2]]
-n_list=[int(n) for n in S[1::2]]
-
-ans=[]
-
-for i in range(N//2):
-	plus=ord(a_list[i]) + n_list[i]**2
-	if plus <= 122:
-		ans.append(chr(plus))
-	elif plus >122:
-		ans.append(chr(plus-122+96))
-print(''.join(ans))
 
 # 첫 제출 답안인데 제출 후 답이 틀린 경우가 많았다.
 # N = int(input())
@@ -37,20 +22,20 @@ print(''.join(ans))
 
 
 # 최종답안
-# import sys
-# input = sys.stdin.readline
+import sys
+input = sys.stdin.readline
 
-# N = int(input())
-# S = list(input().rstrip())
-# res = ''
+N = int(input())
+S = list(input().rstrip())
+res = ''
 
-# for i in range(0, N, 2):
-#     c = S[i]
-#     n = int(S[i + 1])
-#     d = chr((ord(c) - ord('a') + n * n) % 26 + ord('a'))
-#     res += d
+for i in range(0, N, 2):
+    c = S[i]
+    n = int(S[i + 1])
+    d = chr((ord(c) - ord('a') + n * n) % 26 + ord('a'))
+    res += d
 
-# print(res)
+print(res)
 
 """
 exm1
